dc8b9dbcfcaf521f50e3c438ff47afe5ca62d676
orlapez <orlapez@gmail.com>
2025-10-29 08:01:18 -0500

    Users

--- Veterinary.API/Data/DataContext.cs
+++ Veterinary.API/Data/DataContext.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Veterinary.Shared.Entities;
 
 namespace Veterinary.API.Data
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<User>
     {
 
 
--- Veterinary.API/Data/SeedDb.cs
+++ Veterinary.API/Data/SeedDb.cs
@@ -1,14 +1,18 @@
-﻿using Veterinary.Shared.Entities;
+﻿using Veterinary.API.Helpers;
+using Veterinary.Shared.Entities;
+using Veterinary.Shared.Enums;
 
 namespace Veterinary.API.Data
 {
     public class SeedDb
     {
 private readonly DataContext _context;
+private readonly IUserHelper _userHelper;
 
-        public SeedDb(DataContext context)
+        public SeedDb(DataContext context , IUserHelper userHelper)
         {
             _context = context;
+            _userHelper = userHelper;
 
         }
 
@@ -17,10 +21,48 @@ private readonly DataContext _context;
 
             await _context.Database.EnsureCreatedAsync();
             await CheckPetTypesAsync();
+            await CheckRolesAsync();
+            await CheckUserAsync("123", "OAP", "OAP", "CR 78 9687", " tragrammigrako-1348@yopmail.com", UserType.Admin);
+
+
 
 
         }
 
+        private async Task<User> CheckUserAsync(string document, string firstName, string lastName,  string direccion, string email, UserType userType)
+        {
+            var user = await _userHelper.GetUserAsync(email);
+            if (user == null)
+            {
+                user = new User
+                {
+
+                    Document = document,
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Email = email,
+                    Direccion = direccion,
+
+
+                    UserName = email,
+
+
+                    UserType = userType,
+                };
+
+                await _userHelper.AddUserAsync(user, "123456");
+                await _userHelper.AddUserToRoleAsync(user, userType.ToString());
+            }
+
+            return user;
+        }
+
+
+        private async Task CheckRolesAsync()
+        {
+            await _userHelper.CheckRoleAsync(UserType.Admin.ToString());
+            await _userHelper.CheckRoleAsync(UserType.User.ToString());
+        }
 
         private async Task CheckPetTypesAsync()
         {
@@ -29,8 +71,11 @@ private readonly DataContext _context;
                 _context.PetTypes.Add(new PetType { Name = "Dog" });
                 _context.PetTypes.Add(new PetType { Name = "Cat" });
                 _context.PetTypes.Add(new PetType { Name = "Bird" });
-                await _context.SaveChangesAsync();
             }
         }
+
+
+
     }
+
 }
